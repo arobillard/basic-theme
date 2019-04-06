376b8f93c990d8cacf316817f41cc2b57d73ea5d
arobillard <robi0653@algonquinlive.com>
2019-04-05 21:26:23 -0400

    Fix data variable issues

--- _includes/patterns/header/basic.js
+++ _includes/patterns/header/basic.js
@@ -1,15 +1,46 @@
-$('.has-dropdown > a').focus(function() {
-  $(this).siblings('.dropdown').addClass('open');
+// Toggle Nav
+
+$('.tog-nav').on('click', function (e) {
+  e.preventDefault();
+  $('.masthead').toggleClass('nav-open');
 });
 
-$('.has-dropdown > a').focusout(function() {
-  $(this).siblings('.dropdown').removeClass('open');
+$('.nav .icon-link').on('click', function(e) {
+  e.preventDefault();
+  $(this).toggleClass('open');
+  $(this).siblings('.dropdown').toggleClass('open');
+  console.log('sexy');
 });
 
-$('.dropdown a').focus(function() {
-  $(this).parent().parent().addClass('open');
-});
+var x = window.matchMedia("(min-width: 38em)")
+noFocus(x) // Call listener function at run time
+x.addListener(noFocus) // Attach listener function on state changes
 
-$('.dropdown a').focusout(function() {
-  $(this).parent().parent().removeClass('open');
-});
+function noFocus(x) {
+  if (x.matches) {
+
+    $('.m-no-focus').attr('tabindex', '-1');
+    console.log('woot');
+
+    $('.has-dropdown > a').focus(function() {
+      $(this).siblings('.dropdown').addClass('open');
+      console.log('time');
+    });
+
+    $('.has-dropdown > a').focusout(function() {
+      $(this).siblings('.dropdown').removeClass('open');
+      console.log('for');
+    });
+
+    $('.dropdown a').focus(function() {
+      $(this).parent().parent().addClass('open');
+      console.log('every');
+    });
+
+    $('.dropdown a').focusout(function() {
+      $(this).parent().parent().removeClass('open');
+      console.log('one');
+    });
+
+  }
+}
--- js/theme.js
+++ js/theme.js
@@ -15,10 +15,10 @@
 
 // Toggle Nav
 
-$('.tog-nav').on('click', function (e) {
-  e.preventDefault();
-  $('.masthead').toggleClass('nav-open');
-});
+// $('.tog-nav').on('click', function (e) {
+//   e.preventDefault();
+//   $('.masthead').toggleClass('nav-open');
+// });
 
 
 // var my_json = $.getJSON('./theme2.json', function(json) {
@@ -28,19 +28,8 @@ $('.tog-nav').on('click', function (e) {
 //
 // console.log(my_json)
 
-$('.nav .icon-link').on('click', function(e) {
-  e.preventDefault();
-  $(this).toggleClass('open');
-  $(this).siblings('.dropdown').toggleClass('open');
-});
-
-var x = window.matchMedia("(min-width: 38em)")
-noFocus(x) // Call listener function at run time
-x.addListener(noFocus) // Attach listener function on state changes
-
-function noFocus(x) {
-  if (x.matches) {
-    $('.m-no-focus').attr('tabindex', '-1');
-    console.log('woot');
-  }
-}
+// $('.nav .icon-link').on('click', function(e) {
+//   e.preventDefault();
+//   $(this).toggleClass('open');
+//   $(this).siblings('.dropdown').toggleClass('open');
+// });
